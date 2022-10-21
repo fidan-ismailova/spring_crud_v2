0ea7f774f868f10ba5f8a0947dfc4a53911a436b
Fidan Ismayilova <fidan-ismayilova@outlook.com>
2022-10-21 19:20:23 +0400

    refactor SpringCrudV2Application.java -> Application.java

--- src/main/java/az/ingress/spring_crud_v2/Application.java
+++ src/main/java/az/ingress/spring_crud_v2/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCrudV2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCrudV2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
